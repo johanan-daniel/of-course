7d1c98fe47b9c74e83071664b17756fd6cd2a8ca
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2025-04-02 19:19:54 -0400

    initial commit

---
+++ src/main/java/com/example/of_course/OfCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.of_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/of_course/OfCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.of_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
